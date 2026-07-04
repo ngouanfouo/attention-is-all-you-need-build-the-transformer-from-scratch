847bffd079f2e62854a1a5a98a4ef9654fc17266
Durel-J <132607998+ngouanfouo@users.noreply.github.com>
2026-07-04 17:48:03 +0100

    Step 50: apply_log_softmax_over_vocab

--- model.py
+++ model.py
@@ -1065,8 +1065,10 @@ def tie_output_projection_to_token_embeddings(token_embedding_weight):
     # TODO: return an output projection weight tied to the token embedding matrix
     return token_embedding_weight.T
 
-# Step 50 - apply_log_softmax_over_vocab (not yet solved)
-# TODO: implement
+# Step 50 - apply_log_softmax_over_vocab
+def apply_log_softmax_over_vocab(logits):
+    # TODO: Convert decoder logits (B, T, V) into log probabilities over the vocabulary axis.
+    return F.log_softmax(logits,dim=-1)
 
 # Step 51 - run_transformer_forward (not yet solved)
 # TODO: implement
